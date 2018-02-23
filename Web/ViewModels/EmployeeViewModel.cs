fb65e9aeb2efd0f591df5c0eb4684340cfe78d39
Taigrom Andrade <taigrom.andrade@aec.com.br>
2018-02-23 17:42:16 -0300

    inicio projeto

---
+++ Web/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.Models;
+
+namespace Web.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public EmpDetailsModel empDetailModel { get; set; }
+        public EmpAddressModel empAddressModel { get; set; }
+    }
+}
